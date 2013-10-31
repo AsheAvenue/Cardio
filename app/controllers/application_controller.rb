b0d71ea5abfcc0f789e1f843d4ac59a336226cc6
Tim Boisvert <tboisvert@asheavenue.com>
2013-10-31 15:50:51 -0400

    fixed illroots settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+
+  
+  protected
+  
+  def authenticate
+    authenticate_or_request_with_http_basic('Administration') do |username, password|
+      username == 'ashe' && password == 'avenue'
+    end
+  end
+  
 end
