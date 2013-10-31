c11ece569a456cd233f682f629fcfd57e56826bd
Tim Boisvert <tboisvert@asheavenue.com>
2013-10-31 18:26:09 -0400

    server and client views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,17 @@
 class HomeController < ApplicationController
   before_filter :authenticate
+  
   def index
   end
+  
+  def client
+    @slug = params[:slug]
+    @settings = Settings.clients[@slug]
+  end
+  
+  def servers
+    @slug = params[:slug]
+    @settings = Settings.clients[@slug]
+  end
+  
 end
