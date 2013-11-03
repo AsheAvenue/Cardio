3ec18a1a2b58650f19b8538970b9dd07b58295f6
Tim Boisvert <tboisvert@asheavenue.com>
2013-11-03 16:18:22 -0500

    added generic embed type

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,6 +3,8 @@ module HomeHelper
   def generate_embed(type, code, width, height)
     if type == "newrelic"
       "<iframe src=\"https://rpm.newrelic.com/public/charts/#{code}\" width=\"#{width}\" height=\"#{height}\" scrolling=\"no\" frameborder=\"no\"></iframe>"
+    elsif type == "generic"
+      "<iframe src=\"#{code}\" width=\"#{width}\" height=\"#{height}\" scrolling=\"no\" frameborder=\"no\"></iframe>"
     end
   end
   
