be3b6876874d4e8327380d67835c842395b891f9
Tim Boisvert <tboisvert@asheavenue.com>
2013-11-05 10:12:05 -0500

    beginnings of overlay?

--- app/assets/javascripts/cardio.js
+++ app/assets/javascripts/cardio.js
@@ -1,15 +1,29 @@
 var cardio = (function(){
     
-    var self;
+    var self,
+        overlay_enabled = false;
     
     return {
     
         init: function(){
     
             self = this;
-            
+            self.showOverlay();
         },
         
+        showOverlay: function(){
+            if(cardio.overlay_enabled == true) {
+                setTimeout(function () {
+                    alert('overlay here');
+                }, 1000);
+            }
+        },
+        
+        hideOverlay: function() {
+            if(cardio.overlay_enabled == true) {
+                alert('hide overlay');
+            }
+        }
     };
 })();
 
